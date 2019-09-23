98681cafc0d222e5a54e8eee4a4265b7e820e297
Sebastian Vendt <sebastian.vendt@uni-ulm.de>
2019-09-23 22:04:43 +0200

    DAVBWss.sh now screenshooting the next 3 days

--- scripts/DAVBWss.sh
+++ scripts/DAVBWss.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-sat=$(date -dnext-saturday +%Y-%m-%d)
-sun=$(date -dnext-sunday +%Y-%m-%d)
+# the script should be executed ad the beginning of every day (e.g. at 00:05)
+# it will take a screenshot of +1day, +2days and +3days weather
+# sat=$(date -dnext-saturday +%Y-%m-%d)
+# sun=$(date -dnext-sunday +%Y-%m-%d)
+
+day1 = $(date -d"+1day" +%Y-%m-%d)
+day2 = $(date -d"+2day" +%Y-%m-%d)
+day3 = $(date -d"+3day" +%Y-%m-%d)
+
 
 DISPLAY=:1
 export DISPLAY=:1
@@ -10,16 +17,20 @@ Xvfb $DISPLAY -screen 0 1800x1200x24 &
 
 x11_pid=$!
 
-firefox "https://www.alpenverein.de/DAV-Services/Bergwetter/#${sat}|weatherLayer" &> /dev/null &
+firefox "https://www.alpenverein.de/DAV-Services/Bergwetter/#${day1}|weatherLayer" &> /dev/null &
 # wait for page to load
 sleep 2
 xdotool search -name ".*Mozilla Firefox" windowsize 1800 1200
 xdotool search -name ".*Mozilla Firefox" mousemove 0 0
-import -window root -crop 638x398+415+559 ~/weatherSat.jpg
+import -window root -crop 638x398+415+559 /var/www/smartmirror/mountainWeather/weatherday1.jpg
 
-firefox "https://www.alpenverein.de/DAV-Services/Bergwetter/#${sun}|weatherLayer"
+firefox "https://www.alpenverein.de/DAV-Services/Bergwetter/#${day2}|weatherLayer"
 sleep 2
-import -window root -crop 638x398+415+559 ~/weatherSun.jpg
+import -window root -crop 638x398+415+559 /var/www/smartmirror/mountainWeather/weatherday2.jpg
+
+firefox "https://www.alpenverein.de/DAV-Services/Bergwetter/#${day3}|weatherLayer"
+sleep 2
+import -window root -crop 638x398+415+559 /var/www/smartmirror/mountainWeather/weatherday3.jpg
 
 kill $x11_pid
 
