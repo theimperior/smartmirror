cb79e8725c22177c6bb8ac9b7370af120b1d75cd
Sebastian Vendt <sebastian.vendt@uni-ulm.de>
2019-09-24 01:03:47 +0200

    bugfixs in DAVBWss.sh

--- scripts/DAVBWss.sh
+++ scripts/DAVBWss.sh
@@ -1,36 +1,46 @@
-#!/bin/bash
+#!/bin/sh
 # the script should be executed ad the beginning of every day (e.g. at 00:05)
 # it will take a screenshot of +1day, +2days and +3days weather
 # sat=$(date -dnext-saturday +%Y-%m-%d)
 # sun=$(date -dnext-sunday +%Y-%m-%d)
 
-day1 = $(date -d"+1day" +%Y-%m-%d)
-day2 = $(date -d"+2day" +%Y-%m-%d)
-day3 = $(date -d"+3day" +%Y-%m-%d)
+day1=$(date -d"+1day" +%Y-%m-%d)
+day2=$(date -d"+2day" +%Y-%m-%d)
+day3=$(date -d"+3day" +%Y-%m-%d)
 
 
-DISPLAY=:1
-export DISPLAY=:1
+DISPLAY=:99
+export DISPLAY=:99
 export XAUTHORITY=/home/basti/.Xauthority
+load_time=25
 
 Xvfb $DISPLAY -screen 0 1800x1200x24 &
-
 x11_pid=$!
-
+sleep 1
 firefox "https://www.alpenverein.de/DAV-Services/Bergwetter/#${day1}|weatherLayer" &> /dev/null &
+ff_pid=$!
+
 # wait for page to load
-sleep 2
+sleep $load_time
 xdotool search -name ".*Mozilla Firefox" windowsize 1800 1200
 xdotool search -name ".*Mozilla Firefox" mousemove 0 0
-import -window root -crop 638x398+415+559 /var/www/smartmirror/mountainWeather/weatherday1.jpg
+sleep 1
+import -window root -crop 638x398+415+459 /var/www/smartmirror/mountainWeather/weatherday1.jpg
+#import -window root /var/www/smartmirror/mountainWeather/weatherday1.jpg
 
 firefox "https://www.alpenverein.de/DAV-Services/Bergwetter/#${day2}|weatherLayer"
-sleep 2
-import -window root -crop 638x398+415+559 /var/www/smartmirror/mountainWeather/weatherday2.jpg
+sleep $load_time
+import -window root -crop 638x398+415+459 /var/www/smartmirror/mountainWeather/weatherday2.jpg
+#import -window root /var/www/smartmirror/mountainWeather/weatherday2.jpg
 
 firefox "https://www.alpenverein.de/DAV-Services/Bergwetter/#${day3}|weatherLayer"
-sleep 2
-import -window root -crop 638x398+415+559 /var/www/smartmirror/mountainWeather/weatherday3.jpg
+sleep $load_time
+import -window root -crop 638x398+415+459 /var/www/smartmirror/mountainWeather/weatherday3.jpg
+#import -window root /var/www/smartmirror/mountainWeather/weatherday3.jpg
 
+xdotool search -name ".*Mozilla Firefox" windowclose
+
+#kill $ff_pid
+sleep 3
 kill $x11_pid
 
