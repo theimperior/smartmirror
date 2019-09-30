1b4bd8b11f55d3fc866d64c99861bfb0d0fdc0d6
Sebastian Vendt <sebastian.vendt@uni-ulm.de>
2019-09-30 20:49:11 +0200

    adding rainradar

--- scripts/rainradar.sh
+++ scripts/rainradar.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-# the script should be executed ad the beginning of every day (e.g. at 00:05)
-# it will take a screenshot of +1day, +2days and +3days weather
-# sat=$(date -dnext-saturday +%Y-%m-%d)
-# sun=$(date -dnext-sunday +%Y-%m-%d)
 
-d=$(date -d"-2hours" +%Y_%m_%d)
-h=$(date -d"-2hours" +%H)
-m_tmp=$(date +%M)
+d=$(date -d"-2hours-4minutes" +%Y_%m_%d)
+d2=$(date -d"-2hours-4minutes" +%Y%m%d)
+h=$(date -d"-2hours-4minutes" +%H)
+m_tmp=$(date -d"-4minutes" +%M)
+#time=$(date -d"-2hours-4minutes" +%H%M)
 r=$(($m_tmp % 5))
-m=$(($m_tmp - $r - 10))
+m=$(($m_tmp - $r))
 
-
+wget -O /dev/null https://kachelmannwetter.com/de/regenradar/ulm/${d2}-${h}${m}z.html
+sleep 0.5
 wget -O ../rainRadar/rr1.png "https://img3.kachelmannwetter.com/images/data/cache/px250/px250_${d}_265_${h}${m}.png"
